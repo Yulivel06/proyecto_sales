fc9ff122fb96060a151303d9467c53e97a8e56b0
Yulisa Velasquez <yulisap_2009@hotmail.com>
2023-01-05 11:53:21 -0500

    analizando informacion sobre los meses

--- analisis_datos.sql
+++ analisis_datos.sql
@@ -13,15 +13,26 @@ SELECT
     count(product) AS total_product_sold
 FROM sales;
 
+-- promedio por orden
+
+-- total productos
+
+
 -- ¿Cual fue el mejor MES para ventas? ¿Cuanto se ganó por mes?
 
+WITH revenue_month AS (
+    SELECT to_char(order_date, 'month')  AS month, -- To_chart extrae el nombre del mes del año
+    to_char(order_date, 'MM')   AS number_month,
+    SUM(quantity_ordered * price_each) AS revenue_month
+    FROM sales
+    GROUP BY number_month, month, extract(year from order_date)
+    )
 SELECT
-    to_char(order_date, 'month') AS month,      -- To_chart extrae el nombre del mes del año
-    to_char(order_date, 'MM') AS number_month,
-    extract(year from order_date),
-    SUM(quantity_ordered*price_each) AS revenue_month
-FROM sales
-GROUP BY number_month, month, extract(year from order_date);
+    month,
+    number_month,
+    revenue_month,
+    round(revenue_month/(SELECT SUM(quantity_ordered*price_each) FROM sales),2) AS percentage
+FROM revenue_month;
 
 --Ordenes por mes
 
@@ -44,31 +55,31 @@ INNER JOIN estados_eeuu
 GROUP BY city, name
 ORDER BY  num_order DESC;
 
---ciudad con mas ingresos
+--ingresos por estados
 
 SELECT
-    city,
-    name,
-    SUM(quantity_ordered*price_each) as revenue
+    name AS  state,
+        SUM(quantity_ordered*price_each) as revenue_state
 FROM sales
-    INNER JOIN estados_eeuu
-    USING (state)
-GROUP BY city, name
-ORDER BY  revenue DESC;
+INNER JOIN estados_eeuu
+USING (state)
+GROUP BY name;
 
--- Estado con mas ingresos y % de ingresos
+-- city con mas ingresos y % de ingresos
 
 WITH revenue_state as (
     SELECT
+        city,
         name AS  state,
         SUM(quantity_ordered*price_each) as revenue_state
     FROM sales
         INNER JOIN estados_eeuu
         USING (state)
-    GROUP BY name)
+    GROUP BY name,city)
 SELECT
-    state,
-    round(revenue_state/ (SELECT SUM(quantity_ordered*price_each) FROM sales),2) AS percentage
+    concat(city,', ', state) as city,
+    round(revenue_state/ (SELECT SUM(quantity_ordered*price_each) FROM sales),2) AS percentage,
+    revenue_state
 FROM revenue_state
 ORDER BY percentage DESC
 ;
@@ -86,16 +97,67 @@ INNER JOIN estados_eeuu
 GROUP BY order_id,hour , city, name
 ORDER BY total_orders;
 
+-- dia con mas ordenes
+
+SELECT
+    name,
+    to_char(order_date, 'day') AS name_day,
+	date_part('dow', order_date) as num_day,
+    count (distinct (order_id)) AS total_orders
+FROM sales
+INNER JOIN estados_eeuu
+    USING (state)
+GROUP BY order_id, name, num_day, name_day
+ORDER BY total_orders;
+
 
 -- Product best seller
 
-SELECT product, sum(quantity_ordered) AS quantity
-from sales
-GROUP BY  product
+SELECT product,
+           sum(quantity_ordered*price_each) AS revenue,
+           sum(quantity_ordered) AS quantity,
+           price_each
+FROM sales
+GROUP BY  product, price_each
 ORDER BY quantity DESC;
 
 
+-- ¿Qué probabilidad hay de que las próximas personas soliciten el cable de carga USB-C?
 
+SELECT product, 1.0*SUM(quantity_ordered)/
+                (SELECT sum(quantity_ordered)   FROM sales)
+FROM  sales
+WHERE product = 'USB-C Charging Cable'
+GROUP BY product;
+
+
+-- ¿Cuánta probabilidad de que la próxima gente ordene un iPhone?
+
+select product, 1.0*SUM(quantity_ordered)/
+                (SELECT sum(quantity_ordered)   FROM sales)
+from sales
+where product like ('%iPhone%')
+group by  product;
+
+-- ¿Cuánta probabilidad de que la próxima gente ordene Google Phone?
+
+select product, 1.0*SUM(quantity_ordered)/
+                (SELECT sum(quantity_ordered)   FROM sales)
+from sales
+where product like ('%Google%')
+group by  product;
+
+-- ¿Cuánta probabilidad de que otras personas ordenen auriculares con cable?
+
+select product, SUM(quantity_ordered)/
+                (SELECT sum(quantity_ordered) FROM sales)::float
+from sales
+where product = 'Wired Headphones'
+group by product
+;
+
+
+select*from sales;
 
 
 
